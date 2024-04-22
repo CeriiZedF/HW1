5a12cc6046aa1ddf8538f1adf23f6ea52ead667a
CeriiZedF <dasbalan14@gmail.com>
2024-04-22 19:01:22 +0200

    Добавьте файлы проекта.

---
+++ HW1/Pages/CountryInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HW1.Pages
+{
+    public class CountryInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HW1/Pages/RestaurantInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HW1.Pages
+{
+    public class RestaurantInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
